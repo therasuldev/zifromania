75542bddaaa9721924231f19d7b7db1e8cc3ed59
therasuldev <rasul.ramixanov@gmail.com>
2025-03-29 00:29:56 +0400

    Add DifficultyButton widget with animation and dialog for game start

---
+++ lib/presentation/widgets/difficulty_button.dart
@@ -0,0 +1,157 @@
+import 'package:flutter/material.dart';
+import 'package:math_game/main.dart';
+
+import '../../domain/entities/enums.dart';
+
+class DifficultyButton extends StatefulWidget {
+  final String title;
+  final Color color;
+  final GameDifficulty difficulty;
+
+  const DifficultyButton({
+    super.key,
+    required this.title,
+    required this.color,
+    required this.difficulty,
+  });
+
+  @override
+  State<DifficultyButton> createState() => _DifficultyButtonState();
+}
+
+class _DifficultyButtonState extends State<DifficultyButton> with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Color _buttonColor;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      duration: const Duration(milliseconds: 150),
+      vsync: this,
+      lowerBound: 0.9, // Kiçilmə dərəcəsi
+      upperBound: 1.0, // Normal ölçü
+    );
+    _buttonColor = widget.color; // Əsas rəng
+    _animationController.forward(); // Başda böyüyür
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  void _onTapDown(TapDownDetails _) {
+    _animationController.reverse(); // Basanda kiçilir
+    setState(() => _buttonColor = widget.color.withValues(alpha: 0.9));
+  }
+
+  void _onTapUp(TapUpDetails _) {
+    _animationController.forward(); // Buraxanda böyüyür
+    setState(() => _buttonColor = widget.color); // Normal rəngə qayıdır
+
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text("Oyuna Başlamadan Önce"),
+          content: const Text(
+              "Bu modda, oyunun temel dinamikleri ve ipuçları hakkında bilgiler verilecektir. Hazır olduğunuzda 'Tamam' butonuna basarak oyuna başlayabilirsiniz."),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+                builder(BuildContext context) => GameScreen(difficulty: widget.difficulty);
+                Navigator.push(context, MaterialPageRoute(builder: builder));
+              },
+              child: const Text("Tamam"),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _onTapCancel() {
+    _animationController.animateTo(1.0); // Əgər toxunub çıxarsa, normal ölçüyə qayıdır
+    setState(() => _buttonColor = widget.color); // Normal rəngə qayıdır
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTapDown: _onTapDown,
+      onTapUp: _onTapUp,
+      onTapCancel: _onTapCancel,
+      child: AnimatedBuilder(
+        animation: _animationController,
+        builder: (context, child) => Transform.scale(
+          scale: _animationController.value,
+          child: ChildWidget(
+            buttonColor: _buttonColor,
+            widget: widget,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ChildWidget extends StatelessWidget {
+  const ChildWidget({
+    super.key,
+    required Color buttonColor,
+    required this.widget,
+  }) : _buttonColor = buttonColor;
+
+  final Color _buttonColor;
+  final DifficultyButton widget;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      // Konteynerlə düyməyə əlavə kölgə və kənar radius veririk
+      decoration: BoxDecoration(
+        color: _buttonColor,
+        gradient: LinearGradient(
+          colors: [
+            _buttonColor,
+            _buttonColor.withOpacity(0.8),
+          ],
+          begin: Alignment.bottomRight,
+          end: Alignment.topLeft,
+        ),
+        borderRadius: BorderRadius.circular(12),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.5),
+            offset: const Offset(5, 5),
+            blurRadius: 3,
+            spreadRadius: 1,
+          ),
+        ],
+      ),
+      child: Transform(
+        transform: Matrix4.identity()
+          ..setEntry(3, 2, 0.009) // Perspektiv effekti
+          ..rotateX(0.1) // X oxu ətrafında əyilmə
+          ..rotateY(0.05) // Y oxu ətrafında yüngül dönmə
+          ..translate(0.0, -5.0, -10.0), // Bir az geri və yuxarı hərəkət
+        alignment: Alignment.center,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 25),
+          child: Text(
+            widget.title,
+            style: const TextStyle(
+              fontSize: 22,
+              letterSpacing: 1.5,
+              fontFamily: 'Onacona',
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
