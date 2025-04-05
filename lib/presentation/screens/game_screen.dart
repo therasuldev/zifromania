4bd98fd135bd0397087134b79075a6fc992ec4f8
therasuldev <rasul.ramixanov@gmail.com>
2025-04-06 03:34:11 +0400

    Change background image in GameScreen from 'background4.jpg' to 'background.png' for improved visual consistency.

--- lib/presentation/screens/game_screen.dart
+++ lib/presentation/screens/game_screen.dart
@@ -179,7 +179,7 @@ class _GameScreenState extends State<GameScreen> with TickerProviderStateMixin {
       body: Container(
         decoration: const BoxDecoration(
           image: DecorationImage(
-            image: AssetImage('assets/jpg/background4.jpg'),
+            image: AssetImage('assets/jpg/background.png'),
             fit: BoxFit.cover,
           ),
         ),
