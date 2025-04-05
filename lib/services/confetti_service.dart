c712cfa13b3be339412ebde0951f17199c048075
therasuldev <rasul.ramixanov@gmail.com>
2025-04-05 19:06:47 +0400

    Refactor ConfettiService by removing the reset method and adjusting the shouldLoop parameter in buildConfettiWidget to false for improved functionality and clarity.

--- lib/services/confetti_service.dart
+++ lib/services/confetti_service.dart
@@ -3,34 +3,28 @@ import 'package:flutter/material.dart';
 
 class ConfettiService {
   late ConfettiController _confettiController;
-  
+
   ConfettiService({Duration duration = const Duration(seconds: 1)}) {
     _confettiController = ConfettiController(duration: duration);
   }
-  
+
   ConfettiController get controller => _confettiController;
-  
+
   void play() {
     _confettiController.play();
   }
-  
+
   void stop() {
     _confettiController.stop();
-  }
-  
-  void reset() {
-    _confettiController.dispose();
-    _confettiController = ConfettiController(duration: const Duration(seconds: 1));
   }
   
   void dispose() {
     _confettiController.dispose();
   }
-  
+
   Widget buildConfettiWidget({
-    // Alignment alignment = Alignment.center,
     BlastDirectionality blastDirectionality = BlastDirectionality.explosive,
-    bool shouldLoop = true,
+    bool shouldLoop = false,
     List<Color> colors = const [Colors.green, Colors.blue, Colors.pink, Colors.orange, Colors.purple],
   }) {
     return ConfettiWidget(
@@ -38,7 +32,6 @@ class ConfettiService {
       blastDirectionality: blastDirectionality,
       shouldLoop: shouldLoop,
       colors: colors,
-      // alignment: alignment,
     );
   }
 }
