6f2bb30d6b07edfed7b403af9a257bf1a3af261c
therasuldev <rasul.ramixanov@gmail.com>
2025-04-05 19:04:51 +0400

    Refactor AnswerButton widget to enhance visual design by removing gradient background, adjusting button colors, and simplifying layout structure for improved clarity and performance.

--- lib/presentation/widgets/game/answer_button.dart
+++ lib/presentation/widgets/game/answer_button.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import '../../common/grid_painter.dart';
 
 class AnswerButton extends StatelessWidget {
   final int answerValue;
@@ -23,7 +22,7 @@ class AnswerButton extends StatelessWidget {
 
   Color _getButtonColor() {
     if (lastSelectedAnswer == null) {
-      return Colors.blue; // Default state
+      return const Color.fromARGB(0, 29, 45, 40); // Default state
     }
 
     if (answerValue == correctAnswer) {
@@ -61,124 +60,78 @@ class AnswerButton extends StatelessWidget {
             scale: lastSelectedAnswer == index ? buttonAnimationController.value : 1.0,
             child: Container(
               decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  begin: Alignment.topLeft,
-                  end: Alignment.bottomRight,
-                  colors: [
-                    _getButtonColor().withOpacity(0.8),
-                    _getButtonColor(),
-                  ],
-                ),
                 borderRadius: BorderRadius.circular(20),
                 boxShadow: [
                   BoxShadow(
-                    color: _getButtonColor().withOpacity(0.3),
+                    color: _getButtonColor().withOpacity(.1),
                     spreadRadius: 2,
                     blurRadius: 8,
                     offset: const Offset(0, 4),
                   ),
                   BoxShadow(
-                    color: Colors.black.withOpacity(0.2),
+                    color: Colors.black.withOpacity(0.1),
                     spreadRadius: 1,
                     blurRadius: 4,
                     offset: const Offset(0, 2),
                   ),
                 ],
                 border: Border.all(
-                  color: Colors.white.withOpacity(0.3),
+                  color: Colors.white.withOpacity(0.1),
                   width: 2,
                 ),
               ),
-              child: Stack(
-                children: [
-                  // Background Pattern
-                  Positioned.fill(
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(20),
-                      child: CustomPaint(
-                        painter: GridPainter(
-                          color: Colors.white.withOpacity(0.1),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    if (lastSelectedAnswer == index) const Spacer(),
+                    Container(
+                      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                      decoration: BoxDecoration(
+                        color: Colors.white.withOpacity(0.1),
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      child: Text(
+                        answerValue.toString(),
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontFamily: 'Brawler',
+                          letterSpacing: 2,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                          shadows: [
+                            Shadow(
+                              color: Colors.black.withOpacity(0.3),
+                              offset: const Offset(2, 2),
+                              blurRadius: 4,
+                            ),
+                          ],
                         ),
                       ),
                     ),
-                  ),
-                  // Button Content
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 20),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        if (lastSelectedAnswer == index) const Spacer(),
-                        Container(
-                          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                          decoration: BoxDecoration(
-                            color: Colors.white.withOpacity(0.2),
-                            borderRadius: BorderRadius.circular(15),
-                          ),
-                          child: Text(
-                            answerValue.toString(),
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                              fontSize: 24,
-                              fontFamily: 'Brawler',
-                              letterSpacing: 2,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white,
-                              shadows: [
-                                Shadow(
-                                  color: Colors.black.withOpacity(0.3),
-                                  offset: const Offset(2, 2),
-                                  blurRadius: 4,
-                                ),
-                              ],
-                            ),
-                          ),
+                    if (lastSelectedAnswer == index) const Spacer(),
+                    if (lastSelectedAnswer == index)
+                      Container(
+                        padding: const EdgeInsets.all(8),
+                        decoration: BoxDecoration(
+                          color: Colors.white.withOpacity(0.2),
+                          shape: BoxShape.circle,
                         ),
-                        if (lastSelectedAnswer == index) const Spacer(),
-                        if (lastSelectedAnswer == index)
-                          Container(
-                            padding: const EdgeInsets.all(8),
-                            decoration: BoxDecoration(
-                              color: Colors.white.withOpacity(0.2),
-                              shape: BoxShape.circle,
-                            ),
-                            child: Icon(
-                              isLastAnswerCorrect == true ? Icons.check_circle_rounded : Icons.cancel_rounded,
-                              color: Colors.white,
-                              size: 32,
-                            ),
-                          ),
-                      ],
-                    ),
-                  ),
-                  // Corner Decorations
-                  _buildCornerDecoration(top: true, left: true),
-                  _buildCornerDecoration(top: true, left: false),
-                ],
+                        child: Icon(
+                          isLastAnswerCorrect == true ? Icons.check_circle_rounded : Icons.cancel_rounded,
+                          color: Colors.white,
+                          size: 32,
+                        ),
+                      ),
+                  ],
+                ),
               ),
+              // Corner Decorations
             ),
           );
         },
-      ),
-    );
-  }
-
-  Widget _buildCornerDecoration({required bool top, required bool left}) {
-    return Positioned(
-      top: top ? 0 : null,
-      left: left ? 0 : null,
-      bottom: top ? null : 0,
-      right: left ? null : 0,
-      child: Container(
-        width: 30,
-        height: 30,
-        decoration: BoxDecoration(
-          color: Colors.white.withOpacity(0.1),
-          borderRadius: BorderRadius.only(
-            topLeft: top && left ? const Radius.circular(20) : Radius.zero,
-            topRight: top && !left ? const Radius.circular(20) : Radius.zero,
-          ),
-        ),
       ),
     );
   }
