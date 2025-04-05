9ccb53c5813ecfa6e54114f7fdce6d40a7dbda5b
therasuldev <rasul.ramixanov@gmail.com>
2025-04-06 03:33:39 +0400

    Refactor AnswerButton widget to improve button color logic, enhance layout structure, and adjust shadow effects for better visual clarity and user experience.

--- lib/presentation/widgets/game/answer_button.dart
+++ lib/presentation/widgets/game/answer_button.dart
@@ -22,18 +22,18 @@ class AnswerButton extends StatelessWidget {
 
   Color _getButtonColor() {
     if (lastSelectedAnswer == null) {
-      return const Color.fromARGB(0, 29, 45, 40); // Default state
+      return Colors.transparent; // Default state
     }
 
     if (answerValue == correctAnswer) {
-      return Colors.green.shade300; // Correct answer always green
+      return const Color(0xFF1CAC78); // Correct answer always green
     }
 
     if (lastSelectedAnswer == index) {
-      return Colors.red.shade300; // Selected wrong answer in red
+      return const Color(0xFFE32636); // Selected wrong answer in red
     }
 
-    return Colors.blue; // Other buttons remain blue
+    return const Color.fromARGB(0, 29, 45, 40); // Other buttons remain blue
   }
 
   @override
@@ -59,76 +59,48 @@ class AnswerButton extends StatelessWidget {
           return Transform.scale(
             scale: lastSelectedAnswer == index ? buttonAnimationController.value : 1.0,
             child: Container(
+              width: double.infinity,
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(20),
-                boxShadow: [
-                  BoxShadow(
-                    color: _getButtonColor().withOpacity(.1),
-                    spreadRadius: 2,
-                    blurRadius: 8,
-                    offset: const Offset(0, 4),
-                  ),
-                  BoxShadow(
-                    color: Colors.black.withOpacity(0.1),
-                    spreadRadius: 1,
-                    blurRadius: 4,
-                    offset: const Offset(0, 2),
-                  ),
-                ],
+                boxShadow: lastSelectedAnswer != null
+                    ? [
+                        BoxShadow(
+                          color: _getButtonColor(),
+                          spreadRadius: 2,
+                          blurRadius: 8,
+                          offset: const Offset(0, 4),
+                        ),
+                      ]
+                    : [],
                 border: Border.all(
                   color: Colors.white.withOpacity(0.1),
                   width: 2,
                 ),
+                color: lastSelectedAnswer != null ? Colors.black.withOpacity(0.5) : Colors.transparent,
               ),
               child: Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 20),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    if (lastSelectedAnswer == index) const Spacer(),
-                    Container(
-                      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                      decoration: BoxDecoration(
-                        color: Colors.white.withOpacity(0.1),
-                        borderRadius: BorderRadius.circular(15),
-                      ),
-                      child: Text(
-                        answerValue.toString(),
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          fontSize: 24,
-                          fontFamily: 'Brawler',
-                          letterSpacing: 2,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
-                          shadows: [
-                            Shadow(
-                              color: Colors.black.withOpacity(0.3),
-                              offset: const Offset(2, 2),
-                              blurRadius: 4,
-                            ),
-                          ],
-                        ),
+                child: Center(
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                    decoration: BoxDecoration(
+                      color: lastSelectedAnswer != null ? Colors.white.withOpacity(0.1) : Colors.transparent,
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: Text(
+                      answerValue.toString(),
+                      textAlign: TextAlign.center,
+                      style: const TextStyle(
+                        fontSize: 24,
+                        fontFamily: 'Brawler',
+                        letterSpacing: 2,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
                       ),
                     ),
-                    if (lastSelectedAnswer == index) const Spacer(),
-                    if (lastSelectedAnswer == index)
-                      Container(
-                        padding: const EdgeInsets.all(8),
-                        decoration: BoxDecoration(
-                          color: Colors.white.withOpacity(0.2),
-                          shape: BoxShape.circle,
-                        ),
-                        child: Icon(
-                          isLastAnswerCorrect == true ? Icons.check_circle_rounded : Icons.cancel_rounded,
-                          color: Colors.white,
-                          size: 32,
-                        ),
-                      ),
-                  ],
+                  ),
                 ),
               ),
-              // Corner Decorations
             ),
           );
         },
