181008ec757afabdb9f4284332a0fa3ef65e95b8
therasuldev <rasul.ramixanov@gmail.com>
2025-05-21 16:20:55 +0400

    Add GameBloc, GameEvent, and GameState for game management
    - Introduced GameBloc to handle game-related events and states.
    - Created GameEvent enum to define various game events such as starting, ending, and checking answers.
    - Implemented GameState class to represent the state of the game, including score, timer, and question management.
    - Integrated services for question generation and audio feedback, enhancing game functionality and user experience.

---
+++ lib/presentation/state-managment/game/game_event.dart
@@ -0,0 +1,63 @@
+part of 'game_bloc.dart';
+
+enum GameEvents {
+  startGame,
+  endGame,
+  timerTick,
+  checkAnswer,
+  resetGame,
+  showNextQuestion,
+  playAgain,
+  autoAdvanceQuestion, // For True/False mode
+}
+
+class GameEvent {
+  GameEvents? type;
+  dynamic payload;
+
+  GameEvent.startGame({required GameDifficulty difficulty}) {
+    type = GameEvents.startGame;
+    payload = difficulty;
+  }
+
+  GameEvent.endGame() {
+    type = GameEvents.endGame;
+    payload = null;
+  }
+
+  GameEvent.timerTick() {
+    type = GameEvents.timerTick;
+    payload = null;
+  }
+
+  GameEvent.checkAnswer({
+    required MathQuestion question,
+    required int selectedAnswerIndex,
+  }) {
+    type = GameEvents.checkAnswer;
+    payload = {
+      'question': question,
+      'selectedAnswerIndex': selectedAnswerIndex,
+    };
+  }
+
+  GameEvent.resetGame() {
+    type = GameEvents.resetGame;
+    payload = null;
+  }
+
+  GameEvent.showNextQuestion() {
+    type = GameEvents.showNextQuestion;
+    payload = null;
+  }
+
+  GameEvent.playAgain({required GameDifficulty difficulty}) {
+    type = GameEvents.playAgain;
+    payload = difficulty;
+  }
+
+  GameEvent.autoAdvanceQuestion() {
+    type = GameEvents.autoAdvanceQuestion;
+    payload = null;
+  }
+}
