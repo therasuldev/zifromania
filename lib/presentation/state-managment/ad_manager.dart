3160abd0bc6a5d2c1390e9d53810921e4c4e2969
therasuldev <rasul.ramixanov@gmail.com>
2025-05-26 01:20:38 +0400

    Добавить класс AdManager для управления рекламой в игре

---
+++ lib/presentation/state-managment/ad_manager.dart
@@ -0,0 +1,66 @@
+import 'package:zifromania/services/ad_service.dart';
+import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+
+/// Reklam widget'larını idarə etmək üçün sinif
+class AdManager {
+  final AdService _adService = AdService();
+
+  // Oyun üçün reklam göstərmə məntiqini idarə etmək üçün parametrlər
+  final int _gamePlayCount = 0;
+  final int _interstitialAdThreshold = 3; // Hər 3 oyundan sonra interstitial reklam
+
+  BannerAd? get bannerAd => _adService.bannerAd;
+  bool get isBannerAdLoaded => _adService.isBannerAdLoaded;
+
+  /// AdMob-u başlatmaq üçün metod
+  Future<void> initialize() async {
+    await _adService.initialize();
+    _adService.loadBannerAd();
+    _adService.loadInterstitialAd();
+    _adService.loadRewardedAd();
+  }
+
+  /// Banner reklam widget'ını yaratmaq üçün metod
+  Widget getBannerAdWidget() {
+    if (_adService.isBannerAdLoaded && _adService.bannerAd != null) {
+      return Container(
+        alignment: Alignment.center,
+        width: _adService.bannerAd!.size.width.toDouble(),
+        height: _adService.bannerAd!.size.height.toDouble(),
+        child: AdWidget(ad: _adService.bannerAd!),
+      );
+    }
+    // Reklam yüklənməyibsə, boş konteyner göstər
+    return const SizedBox(height: 50);
+  }
+
+/// Oyun bitdikdə reklam göstərmək üçün metod (callback ilə)
+  void showAdAfterGame({VoidCallback? onAdClosed}) {
+    // _gamePlayCount++;
+
+    // Hər _interstitialAdThreshold oyun sonrasında interstitial reklam göstərilir
+    //if (_gamePlayCount % _interstitialAdThreshold == 0) {
+    _adService.showInterstitialAd(onAdClosed: onAdClosed);
+    //}
+  }
+
+  /// Xüsusi mükafat üçün reklam göstərmək üçün metod
+  void showRewardedAdForBonus({
+    required Function(RewardItem reward) onRewardEarned,
+    required Function() onRewardedAdClosed,
+  }) {
+    if (_adService.isRewardedAdReady) {
+      _adService.showRewardedAd(onUserEarnedReward: (adWithoutView, reward) {
+        onRewardEarned(reward);
+      });
+    } else {
+      onRewardedAdClosed();
+    }
+  }
+
+  /// Resursları azad etmək üçün metod
+  void dispose() {
+    _adService.dispose();
+  }
+}
