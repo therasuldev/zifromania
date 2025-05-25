e4fab3097a3ff7cbe0c587733bb081dedde79cc7
therasuldev <rasul.ramixanov@gmail.com>
2025-05-26 01:21:04 +0400

    Упрощение кода в классе AdService: удалены лишние переносы строк, улучшена обработка закрытия межстраничной рекламы и добавлены колбэки для управления состоянием рекламы.

--- lib/services/ad_service.dart
+++ lib/services/ad_service.dart
@@ -5,81 +5,69 @@ import 'package:google_mobile_ads/google_mobile_ads.dart';
 /// Tətbiq üçün reklam xidmətini idarə edən sinif
 class AdService {
   static final AdService _instance = AdService._internal();
-  
+
   factory AdService() => _instance;
-  
+
   AdService._internal();
-  
+
   // Test və ya real reklam ID-ləri üçün flag
   final bool _testMode = kDebugMode;
-  
+
   // Banner reklamları
   BannerAd? _bannerAd;
   bool _isBannerAdLoaded = false;
-  
+
   // Interstitial reklamları
   InterstitialAd? _interstitialAd;
   bool _isInterstitialAdReady = false;
-  
+
   // Reward reklamları
   RewardedAd? _rewardedAd;
   bool _isRewardedAdReady = false;
-  
+
   // Test ID-ləri
-  final String _testBannerAdUnitId = Platform.isAndroid
-      ? 'ca-app-pub-3940256099942544/6300978111'
-      : 'ca-app-pub-3940256099942544/2934735716';
-      
-  final String _testInterstitialAdUnitId = Platform.isAndroid
-      ? 'ca-app-pub-3940256099942544/1033173712'
-      : 'ca-app-pub-3940256099942544/4411468910';
-      
-  final String _testRewardedAdUnitId = Platform.isAndroid
-      ? 'ca-app-pub-3940256099942544/5224354917'
-      : 'ca-app-pub-3940256099942544/1712485313';
-  
+  final String _testBannerAdUnitId = Platform.isAndroid ? 'ca-app-pub-3940256099942544/6300978111' : 'ca-app-pub-3940256099942544/2934735716';
+
+  final String _testInterstitialAdUnitId = Platform.isAndroid ? 'ca-app-pub-3940256099942544/1033173712' : 'ca-app-pub-3940256099942544/4411468910';
+
+  final String _testRewardedAdUnitId = Platform.isAndroid ? 'ca-app-pub-3940256099942544/5224354917' : 'ca-app-pub-3940256099942544/1712485313';
+
   // Real ID-ləri
   // TODO: Buraya öz real adMob ID-lərinizi əlavə edin
-  final String _realBannerAdUnitId = Platform.isAndroid
-      ? 'ca-app-pub-XXXXXXXXXXXXXXXX/XXXXXXXXXX'
-      : 'ca-app-pub-XXXXXXXXXXXXXXXX/XXXXXXXXXX';
-      
-  final String _realInterstitialAdUnitId = Platform.isAndroid
-      ? 'ca-app-pub-XXXXXXXXXXXXXXXX/XXXXXXXXXX'
-      : 'ca-app-pub-XXXXXXXXXXXXXXXX/XXXXXXXXXX';
-      
-  final String _realRewardedAdUnitId = Platform.isAndroid
-      ? 'ca-app-pub-XXXXXXXXXXXXXXXX/XXXXXXXXXX' 
-      : 'ca-app-pub-XXXXXXXXXXXXXXXX/XXXXXXXXXX';
-  
+  final String _realBannerAdUnitId = Platform.isAndroid ? 'ca-app-pub-XXXXXXXXXXXXXXXX/XXXXXXXXXX' : 'ca-app-pub-XXXXXXXXXXXXXXXX/XXXXXXXXXX';
+
+  final String _realInterstitialAdUnitId = Platform.isAndroid ? 'ca-app-pub-XXXXXXXXXXXXXXXX/XXXXXXXXXX' : 'ca-app-pub-XXXXXXXXXXXXXXXX/XXXXXXXXXX';
+
+  final String _realRewardedAdUnitId = Platform.isAndroid ? 'ca-app-pub-XXXXXXXXXXXXXXXX/XXXXXXXXXX' : 'ca-app-pub-XXXXXXXXXXXXXXXX/XXXXXXXXXX';
+
   // Banner ad ID getter
   String get bannerAdUnitId => _testMode ? _testBannerAdUnitId : _realBannerAdUnitId;
-  
+
   // Interstitial ad ID getter
   String get interstitialAdUnitId => _testMode ? _testInterstitialAdUnitId : _realInterstitialAdUnitId;
-  
+
   // Rewarded ad ID getter
   String get rewardedAdUnitId => _testMode ? _testRewardedAdUnitId : _realRewardedAdUnitId;
-  
+
   // Banner ad durumu
   bool get isBannerAdLoaded => _isBannerAdLoaded;
-  
+
   // Interstitial ad durumu
   bool get isInterstitialAdReady => _isInterstitialAdReady;
-  
+
   // Rewarded ad durumu
   bool get isRewardedAdReady => _isRewardedAdReady;
-  
+
   /// AdMob SDK-nı başlatmaq üçün metod
   Future<void> initialize() async {
     await MobileAds.instance.initialize();
   }
-  
+
   /// Banner reklamını yükləmək üçün metod
   void loadBannerAd() {
     _bannerAd = BannerAd(
       adUnitId: bannerAdUnitId,
-      size: AdSize.banner,
+      size: AdSize.leaderboard,
       request: const AdRequest(),
       listener: BannerAdListener(
         onAdLoaded: (_) {
@@ -96,7 +84,7 @@ class AdService {
     );
     _bannerAd?.load();
   }
-  
+
   /// Interstitial reklamını yükləmək üçün metod
   void loadInterstitialAd() {
     InterstitialAd.load(
@@ -107,7 +95,7 @@ class AdService {
           _interstitialAd = ad;
           _isInterstitialAdReady = true;
           debugPrint('Interstitial reklamı uğurla yükləndi');
-          
+
           // Reklamı bağlandıqdan sonra yenidən yükləmək üçün dinləyici əlavə etmək
           _interstitialAd!.fullScreenContentCallback = FullScreenContentCallback(
             onAdDismissedFullScreenContent: (ad) {
@@ -133,17 +121,40 @@ class AdService {
       ),
     );
   }
-  
+
   /// Interstitial reklamını göstərmək üçün metod
-  void showInterstitialAd() {
+  // void showInterstitialAd() {
+  //   if (_isInterstitialAdReady && _interstitialAd != null) {
+  //     _interstitialAd!.show();
+  //   } else {
+  //     debugPrint('Interstitial reklamı hazır deyil, yenidən yüklənir...');
+  //     loadInterstitialAd();
+  //   }
+  // }
+
+  void showInterstitialAd({VoidCallback? onAdClosed}) {
     if (_isInterstitialAdReady && _interstitialAd != null) {
+      _interstitialAd!.fullScreenContentCallback = FullScreenContentCallback(
+        onAdDismissedFullScreenContent: (ad) {
+          ad.dispose();
+          _interstitialAd = null;
+          loadInterstitialAd(); // Yeni reklam yüklə
+          onAdClosed?.call(); // Callback-i çağır
+        },
+        onAdFailedToShowFullScreenContent: (ad, error) {
+          ad.dispose();
+          _interstitialAd = null;
+          loadInterstitialAd();
+          onAdClosed?.call(); // Xəta halında da callback-i çağır
+        },
+      );
       _interstitialAd!.show();
     } else {
-      debugPrint('Interstitial reklamı hazır deyil, yenidən yüklənir...');
-      loadInterstitialAd();
+      // Əgər reklam yoxdursa, dərhal callback-i çağır
+      onAdClosed?.call();
     }
   }
-  
+
   /// Reward reklamını yükləmək üçün metod
   void loadRewardedAd() {
     RewardedAd.load(
@@ -154,7 +165,7 @@ class AdService {
           _rewardedAd = ad;
           _isRewardedAdReady = true;
           debugPrint('Reward reklamı uğurla yükləndi');
-          
+
           // Reklamı bağlandıqdan sonra yenidən yükləmək üçün dinləyici əlavə etmək
           _rewardedAd!.fullScreenContentCallback = FullScreenContentCallback(
             onAdDismissedFullScreenContent: (ad) {
@@ -180,7 +191,7 @@ class AdService {
       ),
     );
   }
-  
+
   /// Reward reklamını göstərmək üçün metod
   void showRewardedAd({required OnUserEarnedRewardCallback onUserEarnedReward}) {
     if (_isRewardedAdReady && _rewardedAd != null) {
@@ -190,10 +201,10 @@ class AdService {
       loadRewardedAd();
     }
   }
-  
+
   /// Banner reklamını əldə etmək üçün getter
   BannerAd? get bannerAd => _bannerAd;
-  
+
   /// Resursları azad etmək üçün metod
   void dispose() {
     _bannerAd?.dispose();
