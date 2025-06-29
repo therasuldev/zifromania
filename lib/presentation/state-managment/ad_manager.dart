f40261e92c264a9a1c35983c64f4b302a2f067a3
therasuldev <rasul.ramixanov@gmail.com>
2025-06-30 01:03:01 +0400

    feat: Добавить поддержку глобальных рекламных статусов и улучшить обработку вознаграждений в AdManager

--- lib/presentation/state-managment/ad_manager.dart
+++ lib/presentation/state-managment/ad_manager.dart
@@ -1,17 +1,24 @@
-import 'package:zifromania/services/ad_service.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'package:zifromania/domain/entities/enums.dart';
+import 'package:zifromania/services/ad_service.dart';
+import 'package:zifromania/services/game_limit_service.dart';
 
 /// Reklam widget'larını idarə etmək üçün sinif
 class AdManager {
   final AdService _adService = AdService();
+  final GameLimitService? _gameLimitService;
 
   // Oyun üçün reklam göstərmə məntiqini idarə etmək üçün parametrlər
-  final int _gamePlayCount = 0;
+  int _gamePlayCount = 0;
   final int _interstitialAdThreshold = 3; // Hər 3 oyundan sonra interstitial reklam
+
+  AdManager({GameLimitService? gameLimitService}) : _gameLimitService = gameLimitService;
 
   BannerAd? get bannerAd => _adService.bannerAd;
   bool get isBannerAdLoaded => _adService.isBannerAdLoaded;
+  bool get isRewardedAdReady => _adService.isRewardedAdReady;
 
   /// AdMob-u başlatmaq üçün metod
   Future<void> initialize() async {
@@ -35,14 +42,17 @@ class AdManager {
     return const SizedBox(height: 50);
   }
 
-/// Oyun bitdikdə reklam göstərmək üçün metod (callback ilə)
+  /// Oyun bitdikdə reklam göstərmək üçün metod (callback ilə)
   void showAdAfterGame({VoidCallback? onAdClosed}) {
-    // _gamePlayCount++;
+    _gamePlayCount++;
 
     // Hər _interstitialAdThreshold oyun sonrasında interstitial reklam göstərilir
-    //if (_gamePlayCount % _interstitialAdThreshold == 0) {
-    _adService.showInterstitialAd(onAdClosed: onAdClosed);
-    //}
+    if (_gamePlayCount % _interstitialAdThreshold == 0) {
+      _adService.showInterstitialAd(onAdClosed: onAdClosed);
+    } else {
+      // Reklam göstərilməyəcəksə, dərhal callback-i çağır
+      onAdClosed?.call();
+    }
   }
 
   /// Xüsusi mükafat üçün reklam göstərmək üçün metod
@@ -57,6 +67,75 @@ class AdManager {
     } else {
       onRewardedAdClosed();
     }
+  }
+
+  /// YENİ: Global flexible games əldə etmək üçün reward reklam göstər
+  Future<bool> showRewardedAdForFlexibleGames({
+    required Function() onRewardEarned,
+    required Function(String error) onError,
+  }) async {
+    if (_gameLimitService == null) {
+      onError('ad.service_unavailable'.tr());
+      return false;
+    }
+
+    // Artıq 3 reklam izləyibsə, daha çox izləyə bilməz
+    if (!_gameLimitService.canWatchAdForReward()) {
+      onError('ad.daily_ads_limit_reached'.tr());
+      return false;
+    }
+
+    if (!_adService.isRewardedAdReady) {
+      onError('ad.ad_not_ready'.tr());
+      return false;
+    }
+
+    try {
+      bool rewardReceived = false;
+
+      _adService.showRewardedAd(
+        onUserEarnedReward: (adWithoutView, reward) async {
+          // Global reklam sayını artır (bu avtomatik flexible games verəcək)
+          await _gameLimitService.incrementGlobalAdWatchedCount();
+          rewardReceived = true;
+          onRewardEarned();
+        },
+      );
+
+      return rewardReceived;
+    } catch (e) {
+      onError('ad.ad_failed'.tr(args: [e.toString()]));
+      return false;
+    }
+  }
+
+  /// YENİ: Global reklam vəziyyətini əldə et
+  Map<String, dynamic>? getGlobalAdStatus() {
+    return _gameLimitService?.getGlobalAdStatus();
+  }
+
+  /// YENİ: Kateqoriya statistikasını əldə et (flexible games daxil)
+  Future<Map<String, dynamic>?> getCategoryStats(GameCategory category) async {
+    return _gameLimitService?.getCategoryStats(category);
+  }
+
+  /// YENİ: Kateqoriyanın tam statusunu əldə et
+  Map<String, dynamic> getCategoryFullStatus(GameCategory category) {
+    if (_gameLimitService == null) {
+      return {
+        'error': 'ad.service_unavailable'.tr(),
+        'canPlay': false,
+      };
+    }
+
+    final categoryStats = _gameLimitService.getCategoryStats(category);
+    final globalAdStatus = _gameLimitService.getGlobalAdStatus();
+
+    return {
+      'category': categoryStats,
+      'globalAds': globalAdStatus,
+      'canPlay': _gameLimitService.canPlayGame(category),
+    };
   }
 
   /// Resursları azad etmək üçün metod
