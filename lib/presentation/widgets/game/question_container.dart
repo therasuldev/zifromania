be4f043cf92e1e22c32951251fa2405f755d2ab9
therasuldev <rasul.ramixanov@gmail.com>
2026-08-13 20:26:19 +0400

    feat: Обновить отображение текста вопроса, добавив FittedBox для улучшения адаптивности

--- lib/presentation/widgets/game/question_container.dart
+++ lib/presentation/widgets/game/question_container.dart
@@ -9,27 +9,28 @@ class GameQuestionContainer extends StatelessWidget {
     return Center(
       child: Padding(
         padding: const EdgeInsets.all(20.0),
-        child: Text(
-          question,
-          textAlign: TextAlign.center,
-          style: const TextStyle(
-            fontSize: 45,
-            // letterSpacing: 2,
-            fontFamily: 'Scabber',
-            fontWeight: FontWeight.bold,
-            color: Colors.white,
-            shadows: [
-              Shadow(
-                color: Colors.black,
-                offset: Offset(3, 3),
-                blurRadius: 6,
-              ),
-              Shadow(
-                color: Colors.black,
-                offset: Offset(-3, -3),
-                blurRadius: 6,
-              ),
-            ],
+        child: FittedBox(
+          child: Text(
+            question,
+            textAlign: TextAlign.center,
+            style: const TextStyle(
+              fontSize: 45,
+              fontFamily: 'Scabber',
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+              shadows: [
+                Shadow(
+                  color: Colors.black,
+                  offset: Offset(3, 3),
+                  blurRadius: 6,
+                ),
+                Shadow(
+                  color: Colors.black,
+                  offset: Offset(-3, -3),
+                  blurRadius: 6,
+                ),
+              ],
+            ),
           ),
         ),
       ),
