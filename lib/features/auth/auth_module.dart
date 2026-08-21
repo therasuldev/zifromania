b72d7e46f94395b565446688a0b65e8db7d22915
therasuldev <rasul.ramixanov@gmail.com>
2026-08-21 15:11:00 +0400

    feat: Добавить модуль аутентификации с использованием Firebase и Riverpod

---
+++ lib/features/auth/auth_module.dart
@@ -0,0 +1,42 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:zifromania/features/auth/data/datasource/auth_remote_datasource_impl.dart';
+import 'package:zifromania/features/auth/domain/repositories/auth_repository.dart';
+import 'package:zifromania/features/auth/domain/usecases/sign_in_with_google_usecase.dart';
+import 'package:zifromania/features/auth/domain/usecases/sign_out_usecase.dart';
+import 'package:zifromania/features/auth/domain/usecases/get_current_user_usecase.dart';
+import 'package:zifromania/services/cache_service.dart';
+
+import 'data/datasource/auth_remote_datasource.dart';
+import 'data/repositories/auth_repository_impl.dart';
+
+final authProvider = Provider<FirebaseAuth>((ref) {
+  return FirebaseAuth.instance;
+});
+
+final secureCacheServiceProvider = Provider<SecureCacheService>((ref) {
+  return SecureCacheService();
+});
+
+final authRemoteDatasourceProvider = Provider<AuthRemoteDatasource>((ref) {
+  return AuthRemoteDatasourceImpl(
+    auth: ref.read(authProvider),
+    cacheService: ref.read(secureCacheServiceProvider),
+  );
+});
+
+final authRepositoryProvider = Provider<AuthRepository>((ref) {
+  return AuthRepositoryImpl(ref.read(authRemoteDatasourceProvider));
+});
+
+final signInWithGoogleUseCaseProvider = Provider<SignInWithGoogleUseCase>((ref) {
+  return SignInWithGoogleUseCase(ref.read(authRepositoryProvider));
+});
+
+final signOutUseCaseProvider = Provider<SignOutUseCase>((ref) {
+  return SignOutUseCase(ref.read(authRepositoryProvider));
+});
+
+final getCurrentUserUseCaseProvider = Provider<GetCurrentUserUseCase>((ref) {
+  return GetCurrentUserUseCase(ref.read(authRepositoryProvider));
+});
