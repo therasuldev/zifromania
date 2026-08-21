67b8921f0ac54b79d058085c04bc05053e0cb010
therasuldev <rasul.ramixanov@gmail.com>
2026-08-21 15:13:16 +0400

    feat: Добавить класс SignInWithGoogleUseCase для аутентификации через Google

---
+++ lib/features/auth/domain/usecases/sign_in_with_google_usecase.dart
@@ -0,0 +1,9 @@
+import 'package:zifromania/features/auth/data/models/user_model.dart';
+import 'package:zifromania/features/auth/domain/repositories/auth_repository.dart';
+
+class SignInWithGoogleUseCase {
+  final AuthRepository _repository;
+  SignInWithGoogleUseCase(this._repository);
+
+  Future<UserModel> call() => _repository.signInWithGoogle();
+}
