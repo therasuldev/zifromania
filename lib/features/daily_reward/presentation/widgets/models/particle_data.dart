1de7025b927a836fad0216b78bf4d6ca3af601b4
therasuldev <rasul.ramixanov@gmail.com>
2026-08-23 20:50:18 +0400

    feat: Add ParticleData model for particle effects

---
+++ lib/features/daily_reward/presentation/widgets/models/particle_data.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class ParticleData {
+  final double angle;
+  final double distance;
+  final double scale;
+  final double delay;
+  final bool isStarType;
+  final Color color;
+  final double size;
+
+  ParticleData({
+    required this.angle,
+    required this.distance,
+    required this.scale,
+    required this.delay,
+    required this.isStarType,
+    required this.color,
+    required this.size,
+  });
+}
