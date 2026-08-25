2e2412151a8d6a64678e14684e24ff643913376d
therasuldev <rasul.ramixanov@gmail.com>
2026-08-25 16:47:08 +0400

    feat: Add multiple use cases for game usage management including flexible games and ad rewards

---
+++ lib/features/game_usage/domain/usecases/can_play_game_usecase.dart
@@ -0,0 +1,19 @@
+import 'package:zifromania/domain/entities/enums.dart';
+import 'package:zifromania/features/game_usage/domain/repositories/game_usage_repositories.dart';
+
+class CanPlayGameUseCase {
+  final GameUsageRepository repository;
+
+  CanPlayGameUseCase({required this.repository});
+
+  bool call(GameCategory category, {bool willPayWithCoin = false}) {
+    final date = DateTime.now();
+    final subscriptionType = repository.getSubscriptionType();
+    final categoryLimit = repository.getCategoryLimit(subscriptionType, category);
+    final dailyRequests = repository.getDailyRequestCount(category, date);
+    final flexibleGames = repository.getFlexibleGamesCount(date);
+
+    // Köhnə koddakı eyni şərt: normal limit, flexible games və ya coin payment
+    return dailyRequests < categoryLimit || flexibleGames > 0 || willPayWithCoin;
+  }
+}
