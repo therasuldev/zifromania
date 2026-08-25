d45fd7d822fa87725d1b7ecc6a8f665fb249a4c2
therasuldev <rasul.ramixanov@gmail.com>
2026-08-25 12:40:06 +0400

    refactor: Simplify update method in GameStatisticsCalculator for better readability

--- lib/features/user/data/helpers/game_statistics_calculator.dart
+++ lib/features/user/data/helpers/game_statistics_calculator.dart
@@ -5,36 +5,20 @@ import '../models/game_update_data.dart';
 class GameStatisticsCalculator {
   const GameStatisticsCalculator();
 
-  GameStats update({
-    required GameStats current,
-    required GameUpdateData data,
-  }) {
+  GameStats update({required GameStats current, required GameUpdateData data}) {
     final currentCategory = current.categoryStats[data.category] ?? const CategoryStats();
-
-    final updatedCategory = _updateCategory(
-      current: currentCategory,
-      data: data,
-    );
+    final updatedCategory = _updateCategory(current: currentCategory, data: data);
 
     final categoriesPlayed = Map<String, int>.from(current.categoriesPlayed);
-
     categoriesPlayed[data.category] = (categoriesPlayed[data.category] ?? 0) + 1;
 
     final categoryStats = Map<String, CategoryStats>.from(current.categoryStats);
-
     categoryStats[data.category] = updatedCategory;
 
     final totalQuestions = current.totalQuestionsAnswered + data.questionsAnswered;
-
     final totalCorrect = current.totalCorrectAnswers + data.correctAnswers;
-
     final totalGames = current.totalGamesPlayed + 1;
-
-    final totalTime = categoryStats.values.fold<int>(
-      0,
-      (sum, item) => sum + item.totalTimeSpent,
-    );
-
+    final totalTime = categoryStats.values.fold<int>(0, (sum, item) => sum + item.totalTimeSpent);
     final averageTime = totalQuestions == 0 ? 0 : totalTime / totalQuestions;
 
     return current.copyWith(
@@ -52,13 +36,9 @@ class GameStatisticsCalculator {
     required GameUpdateData data,
   }) {
     final gamesPlayed = current.gamesPlayed + 1;
-
     final questionsAnswered = current.questionsAnswered + data.questionsAnswered;
-
     final correctAnswers = current.correctAnswers + data.correctAnswers;
-
     final totalTimeSpent = current.totalTimeSpent + data.gameTimeInSeconds;
-
     final averageTime = questionsAnswered == 0 ? 0 : totalTimeSpent / questionsAnswered;
 
     return current.copyWith(
