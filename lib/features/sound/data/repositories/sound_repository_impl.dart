a71ace98e6d0e7068e7808299e8c862d1403967c
therasuldev <rasul.ramixanov@gmail.com>
2026-08-26 23:00:19 +0400

    refactor: Update SoundRepositoryImpl methods to use async/await for better readability

--- lib/features/sound/data/repositories/sound_repository_impl.dart
+++ lib/features/sound/data/repositories/sound_repository_impl.dart
@@ -2,53 +2,53 @@ import 'package:zifromania/core/enum/haptic_feedback_type.dart';
 import 'package:zifromania/features/sound/data/datasource/sound_local_datasource.dart';
 import 'package:zifromania/features/sound/domain/repositories/sound_repository.dart';
 
-class SoundRepositoryImpl implements SoundRepository {
+final class SoundRepositoryImpl implements SoundRepository {
   final SoundLocalDataSource localDataSource;
 
   SoundRepositoryImpl({required this.localDataSource});
 
   @override
-  Future<void> init() {
-    return localDataSource.init();
+  Future<void> init() async {
+    await localDataSource.init();
   }
 
   @override
-  Future<void> pauseBackgroundMusic() {
-    return localDataSource.pauseBackgroundMusic();
+  Future<void> pauseBackgroundMusic() async {
+    await localDataSource.pauseBackgroundMusic();
   }
 
   @override
-  Future<void> playBackgroundMusic(String assetPath) {
-    return localDataSource.playBackgroundMusic(assetPath);
+  Future<void> playBackgroundMusic(String assetPath) async {
+    await localDataSource.playBackgroundMusic(assetPath);
   }
 
   @override
-  Future<void> playSoundEffect(String assetPath) {
-    return localDataSource.playSoundEffect(assetPath);
+  Future<void> playSoundEffect(String assetPath) async {
+    await localDataSource.playSoundEffect(assetPath);
   }
 
   @override
-  Future<void> resumeBackgroundMusic() {
-    return localDataSource.resumeBackgroundMusic();
+  Future<void> resumeBackgroundMusic() async {
+    await localDataSource.resumeBackgroundMusic();
   }
 
   @override
-  Future<void> stopBackgroundMusic() {
-    return localDataSource.stopBackgroundMusic();
+  Future<void> stopBackgroundMusic() async {
+    await localDataSource.stopBackgroundMusic();
   }
 
   @override
-  Future<void> vibrate(int durationInMilliseconds) {
-    return localDataSource.vibrate(durationInMilliseconds);
+  Future<void> vibrate(int durationInMilliseconds) async {
+    await localDataSource.vibrate(durationInMilliseconds);
   }
 
   @override
-  Future<void> dispose() {
-    return localDataSource.dispose();
+  Future<void> dispose() async {
+    await localDataSource.dispose();
   }
 
   @override
-  Future<void> hapticFeedback(HapticFeedbackType type) {
-    return localDataSource.hapticFeedback(type);
+  Future<void> hapticFeedback(HapticFeedbackType type) async {
+    await localDataSource.hapticFeedback(type);
   }
 }
