4ee5cc26e6501d4cef9b235281bf4a45c4e3642b
therasuldev <rasul.ramixanov@gmail.com>
2026-08-27 22:21:28 +0400

    fix: Update import path for UserModel in auth_provider.dart

--- lib/features/auth/presentation/providers/auth_provider.dart
+++ lib/features/auth/presentation/providers/auth_provider.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:zifromania/features/auth/auth_module.dart';
-import 'package:zifromania/features/auth/data/models/user_model.dart';
+import 'package:zifromania/features/user/data/models/user_model.dart';
 
 class AuthNotifier extends AsyncNotifier<UserModel?> {
   @override
