8c7694b4e855a33f2777a04d9ac48ce8b7fe3af2
therasuldev <rasul.ramixanov@gmail.com>
2026-08-27 22:19:30 +0400

    fix: Update import path for UserModel in auth_repository_impl.dart

--- lib/features/auth/data/repositories/auth_repository_impl.dart
+++ lib/features/auth/data/repositories/auth_repository_impl.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:zifromania/features/auth/data/datasource/auth_remote_datasource.dart';
-import 'package:zifromania/features/auth/data/models/user_model.dart';
+import 'package:zifromania/features/user/data/models/user_model.dart';
 import 'package:zifromania/features/auth/domain/repositories/auth_repository.dart';
 
 class AuthRepositoryImpl implements AuthRepository {
