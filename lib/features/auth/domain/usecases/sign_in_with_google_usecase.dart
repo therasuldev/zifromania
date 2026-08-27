89a7a9252598224b9f464ad9a79a56eb48cce801
therasuldev <rasul.ramixanov@gmail.com>
2026-08-27 22:20:00 +0400

    fix: Update import path for UserModel in sign_in_with_google_usecase.dart

--- lib/features/auth/domain/usecases/sign_in_with_google_usecase.dart
+++ lib/features/auth/domain/usecases/sign_in_with_google_usecase.dart
@@ -1,4 +1,4 @@
-import 'package:zifromania/features/auth/data/models/user_model.dart';
+import 'package:zifromania/features/user/data/models/user_model.dart';
 import 'package:zifromania/features/auth/domain/repositories/auth_repository.dart';
 
 class SignInWithGoogleUseCase {
