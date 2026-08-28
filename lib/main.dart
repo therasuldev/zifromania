dca2e9d9ccd35bce3cf8a4cc9bc0cf835a288b32
therasuldev <rasul.ramixanov@gmail.com>
2026-08-28 23:40:06 +0400

    feat: Refactor main.dart to integrate Riverpod and initialize services

--- lib/main.dart
+++ lib/main.dart
@@ -2,34 +2,43 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:zifromania/app.dart';
+import 'package:zifromania/core/services/notification_service.dart';
+import 'package:zifromania/core/providers/shared_prefs_provider.dart';
 import 'package:zifromania/firebase_options.dart';
-import 'package:zifromania/presentation/providers/bloc_providers.dart';
-import 'package:zifromania/presentation/providers/repository_providers.dart';
-import 'package:zifromania/services/services_init.dart';
-import 'services/sound_service.dart';
+
+import 'core/services/daily_reward_bg_service.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-
   await dotenv.load();
-  await initializeServices();
+
+  // Retrieve the SharedPreferences instance.
+  final sharedPreferences = await SharedPreferences.getInstance();
+
+  // Create the main ProviderContainer and override SharedPreferences.
+  final container = ProviderContainer(
+    overrides: [sharedPreferencesProvider.overrideWithValue(sharedPreferences)],
+  );
+
+  // Initialize NotificationService using the Riverpod container.
+  await container.read(notificationServiceProvider).init();
+  // Initialize DailyRewardBgService to set up background tasks and notifications.
+  await DailyRewardBgService.initWorkManager();
 
   runApp(
-    EasyLocalization(
-      supportedLocales: const [Locale('en'), Locale('tr')],
-      path: 'assets/translations',
-      fallbackLocale: const Locale('en'),
-      child: MultiRepositoryProvider(
-        providers: repositoryProviders,
-        child: MultiBlocProvider(
-          providers: blocProviders,
-          child: ZifroMania(soundService: locator.get<SoundService>()),
-        ),
+    UncontrolledProviderScope(
+      container: container,
+      child: EasyLocalization(
+        supportedLocales: const [Locale('en'), Locale('tr')],
+        path: 'assets/translations',
+        fallbackLocale: const Locale('en'),
+        child: const ZifroMania(),
       ),
     ),
   );
