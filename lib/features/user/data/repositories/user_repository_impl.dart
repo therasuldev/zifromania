cc969e52dab756d03cd35486e647c4e90c7d5b85
therasuldev <rasul.ramixanov@gmail.com>
2026-08-28 16:19:42 +0400

    feat: Enhance UserRepositoryImpl to cache user data and handle local fallback

--- lib/features/user/data/repositories/user_repository_impl.dart
+++ lib/features/user/data/repositories/user_repository_impl.dart
@@ -1,71 +1,80 @@
+import 'package:zifromania/features/user/data/datasource/user_local_data_source.dart';
 import 'package:zifromania/features/user/data/datasource/user_remote_datasource.dart';
 import 'package:zifromania/features/user/data/models/user_model.dart';
 import 'package:zifromania/features/user/domain/entities/user_entity.dart';
 import 'package:zifromania/features/user/domain/repositories/user_repository.dart';
 
 class UserRepositoryImpl implements UserRepository {
-  const UserRepositoryImpl({required this.remoteDataSource});
+  const UserRepositoryImpl({
+    required this.remoteDataSource,
+    required this.localDataSource,
+  });
 
   final UserRemoteDataSource remoteDataSource;
+  final UserLocalDataSource localDataSource;
 
   @override
-  Future<void> createUserProfile({required UserEntity user}) {
-    return remoteDataSource.createUserProfile(
+  Future<void> createUserProfile({required UserEntity user}) async {
+    await remoteDataSource.createUserProfile(
       user: UserModel.fromEntity(user),
     );
   }
 
   @override
   Future<UserEntity> getUser({required String uid}) async {
-    final user = await remoteDataSource.getUser(uid: uid);
+    try {
+      final user = await remoteDataSource.getUser(uid: uid);
 
-    return user.toEntity();
+      await localDataSource.cacheUser(user);
+
+      return user.toEntity();
+    } catch (e) {
+      final cachedUser = await localDataSource.getUser();
+
+      if (cachedUser != null && cachedUser.uid == uid) {
+        return cachedUser.toEntity();
+      }
+
+      rethrow;
+    }
   }
 
   @override
   Stream<UserEntity> watchUser({required String uid}) {
-    return remoteDataSource.watchUser(uid: uid).map(
-          (user) => user.toEntity(),
-        );
+    return remoteDataSource.watchUser(uid: uid).map((user) {
+      localDataSource.cacheUser(user);
+
+      return user.toEntity();
+    });
   }
 
   @override
-  Future<void> deleteUser({required String uid}) {
-    return remoteDataSource.deleteUser(uid: uid);
+  Future<void> deleteUser({required String uid}) async {
+    await remoteDataSource.deleteUser(uid: uid);
+    await localDataSource.clearUser();
   }
 
   @override
-  Future<UserEntity> addCoins({
-    required String uid,
-    required int amount,
-  }) async {
-    final user = await remoteDataSource.addCoins(
-      uid: uid,
-      amount: amount,
-    );
+  Future<UserEntity> addCoins({required String uid, required int amount}) async {
+    final user = await remoteDataSource.addCoins(uid: uid, amount: amount);
+    await localDataSource.cacheUser(user);
 
     return user.toEntity();
   }
 
   @override
-  Future<void> spendCoins({
-    required String uid,
-    required int amount,
-  }) {
-    return remoteDataSource.spendCoins(
-      uid: uid,
-      amount: amount,
-    );
+  Future<UserEntity> spendCoins({required String uid, required int amount}) async {
+    final user = await remoteDataSource.spendCoins(uid: uid, amount: amount);
+    await localDataSource.cacheUser(user);
+
+    return user.toEntity();
   }
 
   @override
-  Future<void> addXp({
-    required String uid,
-    required int xpEarned,
-  }) {
-    return remoteDataSource.addXp(
-      uid: uid,
-      xpEarned: xpEarned,
-    );
+  Future<UserEntity> addXp({required String uid, required int xpEarned}) async {
+    final user = await remoteDataSource.addXp(uid: uid, xpEarned: xpEarned);
+    await localDataSource.cacheUser(user);
+
+    return user.toEntity();
   }
 }
