827ebe4feb9fbe790026aa522878a4f0844e176f
therasuldev <rasul.ramixanov@gmail.com>
2026-08-29 00:24:58 +0400

    feat: Refactor main.dart to initialize SharedPreferences and RewardedAdService

--- lib/main.dart
+++ lib/main.dart
@@ -4,13 +4,14 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:zifromania/app.dart';
 import 'package:zifromania/core/services/notification_service.dart';
-import 'package:zifromania/core/providers/shared_prefs_provider.dart';
+import 'package:zifromania/core/providers/shared_preferences_provider.dart';
 import 'package:zifromania/firebase_options.dart';
 
 import 'core/services/daily_reward_bg_service.dart';
+import 'core/services/rewarded_ad_service.dart';
+import 'core/services/shared_preferences_service.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -18,13 +19,20 @@ void main() async {
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await dotenv.load();
 
-  // Retrieve the SharedPreferences instance.
-  final sharedPreferences = await SharedPreferences.getInstance();
+  // Initialize SharedPreferences
+  final sharedPreferences = PreferencesService();
+  await sharedPreferences.init();
+
+  // Initialize RewardedAdService
+  final adService = RewardedAdService();
+  await adService.initialize();
 
   // Create the main ProviderContainer and override SharedPreferences.
-  final container = ProviderContainer(
-    overrides: [sharedPreferencesProvider.overrideWithValue(sharedPreferences)],
-  );
+  final overrides = [
+    sharedPreferencesProvider.overrideWithValue(sharedPreferences),
+    rewardedAdServiceProvider.overrideWithValue(adService),
+  ];
+  final container = ProviderContainer(overrides: overrides);
 
   // Initialize NotificationService using the Riverpod container.
   await container.read(notificationServiceProvider).init();
