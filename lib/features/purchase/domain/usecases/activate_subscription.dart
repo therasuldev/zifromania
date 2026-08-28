a98b3a5931f8faab78f953862f5aec7551e1599d
therasuldev <rasul.ramixanov@gmail.com>
2026-08-28 16:15:50 +0400

    feat: Add ActivateSubscriptionUseCase for managing subscription activation

---
+++ lib/features/purchase/domain/usecases/activate_subscription.dart
@@ -0,0 +1,15 @@
+import 'package:zifromania/features/user/domain/entities/subscription_entity.dart';
+import 'package:zifromania/features/user/domain/repositories/subscription_repository.dart';
+
+class ActivateSubscriptionUseCase {
+  ActivateSubscriptionUseCase({required this.repository});
+
+  final SubscriptionRepository repository;
+
+  Future<void> call({
+    required String uid,
+    required SubscriptionEntity subscription,
+  }) async {
+    await repository.updateSubscriptionDetails(uid: uid, subscription: subscription);
+  }
+}
