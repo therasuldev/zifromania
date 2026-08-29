d60a7bd05020e7557660723c300eadd9f5bbc6a4
therasuldev <rasul.ramixanov@gmail.com>
2026-08-29 14:47:22 +0400

    fix: Update secureCacheServiceProvider to use SecureStorageService instead of SecureCacheService

--- lib/features/auth/auth_module.dart
+++ lib/features/auth/auth_module.dart
@@ -1,17 +1,17 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:zifromania/core/providers/firebase_provider.dart';
+import 'package:zifromania/core/services/secure_storage_service.dart';
 import 'package:zifromania/features/auth/data/datasource/auth_remote_datasource_impl.dart';
 import 'package:zifromania/features/auth/domain/repositories/auth_repository.dart';
 import 'package:zifromania/features/auth/domain/usecases/sign_in_with_google_usecase.dart';
 import 'package:zifromania/features/auth/domain/usecases/sign_out_usecase.dart';
 import 'package:zifromania/features/auth/domain/usecases/get_current_user_usecase.dart';
-import 'package:zifromania/services/cache_service.dart';
 
 import 'data/datasource/auth_remote_datasource.dart';
 import 'data/repositories/auth_repository_impl.dart';
 
-final secureCacheServiceProvider = Provider<SecureCacheService>((ref) {
-  return SecureCacheService();
+final secureCacheServiceProvider = Provider<SecureStorageService>((ref) {
+  return const SecureStorageService();
 });
 
 final authRemoteDatasourceProvider = Provider<AuthRemoteDatasource>((ref) {
