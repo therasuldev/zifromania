bd9ea77a8ab1617dff94fecdef77aa930b714897
therasuldev <rasul.ramixanov@gmail.com>
2026-08-29 14:45:22 +0400

    feat: Add sharedPreferencesProvider to manage SharedPreferences service

---
+++ lib/core/providers/shared_preferences_provider.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:zifromania/core/services/shared_preferences_service.dart';
+
+final sharedPreferencesProvider = Provider<PreferencesService>((ref) {
+  throw UnimplementedError('Initialize preferencesProvider in ProviderContainer overrides');
+});
