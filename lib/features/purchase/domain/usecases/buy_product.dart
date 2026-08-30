22fea54f0cc4096ca4ed9f488a42e1c51f5c0e99
therasuldev <rasul.ramixanov@gmail.com>
2026-08-30 20:18:17 +0400

    fix: Update BuyProductUseCase to use StoreRepository instead of PurchaseRepository

--- lib/features/purchase/domain/usecases/buy_product.dart
+++ lib/features/purchase/domain/usecases/buy_product.dart
@@ -1,9 +1,9 @@
-import 'package:zifromania/features/purchase/domain/repositories/purchase_repository.dart';
+import 'package:zifromania/features/purchase/domain/repositories/store_repository.dart';
 
 class BuyProductUseCase {
   BuyProductUseCase({required this.repository});
 
-  final PurchaseRepository repository;
+  final StoreRepository repository;
 
   Future<bool> call(String productId) {
     return repository.buyProduct(productId);
