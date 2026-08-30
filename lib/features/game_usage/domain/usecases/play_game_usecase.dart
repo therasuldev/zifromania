0d74a82354d238286f46fda4d058e377aec53a68
therasuldev <rasul.ramixanov@gmail.com>
2026-08-30 20:17:06 +0400

    feat: Implement PlayGameUseCase to manage game play logic and daily limits

---
+++ lib/features/game_usage/domain/usecases/play_game_usecase.dart
@@ -0,0 +1,27 @@
+import 'package:zifromania/domain/entities/enums.dart';
+import 'package:zifromania/features/game_usage/domain/repositories/game_usage_repositories.dart';
+
+final class PlayGameUseCase {
+  const PlayGameUseCase({required this.repository});
+
+  final GameUsageRepository repository;
+
+  Future<void> call(GameCategory category, {bool paidWithCoin = false}) async {
+    final date = DateTime.now();
+
+    if (paidWithCoin) {
+      // Coin ilə ödəniş olunubsa günlük limitə toxunulmur
+      return;
+    }
+
+    final flexibleGames = repository.getFlexibleGamesCount(date);
+    if (flexibleGames > 0) {
+      // Əgər flexible game varsa, ondan 1 dənə düşürük
+      await repository.setFlexibleGamesCount(date, flexibleGames - 1);
+    } else {
+      // Əgər flexible game yoxdursa, günlük limit sayını artırırıq
+      final currentRequests = repository.getDailyRequestCount(category, date);
+      await repository.setDailyRequestCount(category, date, currentRequests + 1);
+    }
+  }
+}
