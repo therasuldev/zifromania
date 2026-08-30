c7ae6ebe75daecb126a9ce0b287a81ea650260f0
therasuldev <rasul.ramixanov@gmail.com>
2026-08-30 20:19:09 +0400

    feat: Add TitleModel class for title data representation and mapping

---
+++ lib/features/title/data/models/title_model.dart
@@ -0,0 +1,34 @@
+import '../../domain/entities/title_entity.dart';
+
+class TitleModel extends TitleEntity {
+  const TitleModel({
+    required super.id,
+    required super.key,
+    required super.name,
+    required super.description,
+    required super.iconUrl,
+    required super.requirements,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'key': key,
+      'name': name,
+      'description': description,
+      'iconUrl': iconUrl,
+      'requirements': requirements,
+    };
+  }
+
+  factory TitleModel.fromMap(Map<String, dynamic> map, String documentId) {
+    return TitleModel(
+      id: documentId,
+      key: map['key'] ?? '',
+      name: map['name'] ?? '',
+      description: map['description'] ?? '',
+      iconUrl: map['iconUrl'] ?? '',
+      requirements: Map<String, dynamic>.from(map['requirements'] ?? {}),
+    );
+  }
+}
