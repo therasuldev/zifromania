7373c82822578a0bc0fa689941e7353519df6a09
therasuldev <rasul.ramixanov@gmail.com>
2026-08-30 20:17:34 +0400

    feat: Implement StoreRepositoryImpl for managing store product data and purchases

---
+++ lib/features/purchase/data/repositories/store_repository_impl.dart
@@ -0,0 +1,65 @@
+import 'package:zifromania/features/purchase/data/datasource/store_datasource.dart';
+import 'package:zifromania/features/purchase/domain/entities/purchase_result_entity.dart';
+import 'package:zifromania/features/purchase/domain/entities/store_product_entity.dart';
+import 'package:zifromania/features/purchase/domain/repositories/store_repository.dart';
+
+class StoreRepositoryImpl implements StoreRepository {
+  const StoreRepositoryImpl({required this.storeDataSource});
+
+  final StoreDataSource storeDataSource;
+
+  @override
+  Future<bool> isAvailable() {
+    return storeDataSource.isAvailable();
+  }
+
+  @override
+  Future<List<StoreProductEntity>> loadProducts() async {
+    final products = await storeDataSource.loadProducts();
+
+    return products
+        .map(
+          (product) => StoreProductEntity(
+            id: product.id,
+            title: product.title,
+            description: product.description,
+            price: product.price,
+            isSubscription: _isSubscription(product.id),
+          ),
+        )
+        .toList();
+  }
+
+  @override
+  Future<bool> buyProduct(String productId) async {
+    final products = await storeDataSource.loadProducts();
+
+    final product = products.firstWhere(
+      (e) => e.id == productId,
+    );
+
+    return storeDataSource.buyProduct(product.id);
+  }
+
+  @override
+  Future<void> restorePurchases() {
+    return storeDataSource.restorePurchases();
+  }
+
+  @override
+  Stream<PurchaseEntity> watchPurchases() {
+    return storeDataSource.purchaseStream.expand(
+      (purchaseList) => purchaseList.map(
+        (purchase) => PurchaseEntity(
+          productId: purchase.productID,
+          purchaseId: purchase.purchaseID,
+          status: purchase.status,
+        ),
+      ),
+    );
+  }
+
+  bool _isSubscription(String productId) {
+    return productId.startsWith('subscription_');
+  }
+}
