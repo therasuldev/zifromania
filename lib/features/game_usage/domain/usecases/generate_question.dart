8e1e6af885cb0ccad3e92b21a7efe7eb2ae4bf96
therasuldev <rasul.ramixanov@gmail.com>
2026-08-30 20:17:54 +0400

    refactor: Improve cancel token handling and streamline question generation logic

--- lib/features/game_usage/domain/usecases/generate_question.dart
+++ lib/features/game_usage/domain/usecases/generate_question.dart
@@ -1,11 +1,10 @@
-// features/game/domain/usecases/generate_questions_usecase.dart
 import 'package:easy_localization/easy_localization.dart';
 import 'package:zifromania/app_exception.dart';
+import 'package:zifromania/core/utils/cancel_token.dart';
 import 'package:zifromania/domain/entities/enums.dart';
 import 'package:zifromania/domain/entities/math_question.dart';
 import 'package:zifromania/features/game_usage/domain/repositories/question_repository.dart';
 import 'package:zifromania/features/user/domain/repositories/user_repository.dart';
-import 'package:zifromania/presentation/state-managment/game/game_bloc.dart';
 
 import 'can_play_game_usecase.dart';
 import 'play_game_usecase.dart';
@@ -31,10 +30,8 @@ final class GenerateQuestionsUseCase {
     bool paidWithCoin = false,
     int coinCost = 10,
   }) async {
-    // 1. Cancel Token yoxlanışı
-    if (cancelToken?.isCancelled == true) {
-      throw Exception('Operation cancelled');
-    }
+    // 1. Əməliyyatın başında yoxlanış
+    cancelToken?.throwIfCancelled();
 
     // 2. Oynamaq hüququnun yoxlanılması
     final canPlay = canPlayGameUseCase(gameCategory, willPayWithCoin: paidWithCoin);
@@ -45,30 +42,30 @@ final class GenerateQuestionsUseCase {
       );
     }
 
-    // 3. Əgər Coin ilə ödəmə seçilibsə, balansı çıxmaq
-    if (paidWithCoin) {
-      await userRepository.spendCoins(uid: userId, amount: coinCost);
-    }
-
-    // 4. Fayldan sualları yükləmək
+    // 3. Sualları öncədən yükləyirik (pul çıxılmadan öncə)
     final allQuestions = await questionRepository.getQuestions(gameCategory);
+    cancelToken?.throwIfCancelled();
 
-    // 5. 7 saniyəlik süni gözləmə (Cancel Token dəstəyi ilə)
+    // 4. 7 saniyəlik süni gözləmə
+    // Qeyd: 70 dəfə loop yerinə təkrarlanan hissəni sadələşdirə bilərsiniz
     for (int i = 0; i < 70; i++) {
       if (cancelToken?.isCancelled == true) {
-        throw Exception('Operation cancelled');
+        throw const OperationCanceledException();
       }
       await Future.delayed(const Duration(milliseconds: 100));
     }
 
-    if (cancelToken?.isCancelled == true) {
-      throw Exception('Operation cancelled');
+    // Pul çıxılmadan sonuncu yoxlanış
+    cancelToken?.throwIfCancelled();
+
+    // 5. YALNIZ hər şey uğurla tamamlandıqdan sonra Coin çıxılır və istifadə statistikası yenilənir
+    if (paidWithCoin) {
+      await userRepository.spendCoins(uid: userId, amount: coinCost);
     }
 
-    // 6. Oynanış limit statistikalarını yeniləmək
     await playGameUseCase(gameCategory, paidWithCoin: paidWithCoin);
 
-    // 7. Sualları qarışdırıb qaytarmaq
+    // 6. Sualları qaytarmaq
     final list = List<MathQuestion>.from(allQuestions)..shuffle();
     return list.take(questionCount).toList();
   }
