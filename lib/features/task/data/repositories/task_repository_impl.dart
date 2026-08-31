38e26b77d293c732387d7b936deabc17f21d6683
therasuldev <rasul.ramixanov@gmail.com>
2026-08-31 10:01:56 +0400

    feat: Implement TaskRepositoryImpl for task data operations

---
+++ lib/features/task/data/repositories/task_repository_impl.dart
@@ -0,0 +1,19 @@
+import 'package:zifromania/features/task/data/datasource/task_datasource.dart';
+
+import '../../domain/entities/task_entity.dart';
+import '../../domain/repositories/task_repository.dart';
+
+final class TaskRepositoryImpl implements TaskRepository {
+  const TaskRepositoryImpl({required this.remoteDataSource});
+
+  final TaskRemoteDataSource remoteDataSource;
+
+  @override
+  Future<List<TaskEntity>> getAllTasks() => remoteDataSource.getAllTasks();
+
+  @override
+  Future<TaskEntity> getTaskById(String taskId) => remoteDataSource.getTaskById(taskId);
+
+  @override
+  Future<List<TaskEntity>> getTasksByIds(List<String> taskIds) => remoteDataSource.getTasksByIds(taskIds);
+}
