1b6bc78c2eafbe8aadf9a3c114462036c6f9d96e
therasuldev <rasul.ramixanov@gmail.com>
2026-08-31 10:01:33 +0400

    fix: Update LoadProductsUseCase to use StoreRepository instead of PurchaseRepository

--- lib/features/purchase/domain/usecases/load_products.dart
+++ lib/features/purchase/domain/usecases/load_products.dart
@@ -1,10 +1,10 @@
 import 'package:zifromania/features/purchase/domain/entities/store_product_entity.dart';
-import 'package:zifromania/features/purchase/domain/repositories/purchase_repository.dart';
+import 'package:zifromania/features/purchase/domain/repositories/store_repository.dart';
 
 class LoadProductsUseCase {
   LoadProductsUseCase({required this.repository});
 
-  final PurchaseRepository repository;
+  final StoreRepository repository;
 
   Future<List<StoreProductEntity>> call() {
     return repository.loadProducts();
