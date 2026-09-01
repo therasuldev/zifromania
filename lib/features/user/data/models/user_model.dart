91f6e41f22c94a9d5261dc1c797b3b1a0e3e3730
therasuldev <rasul.ramixanov@gmail.com>
2026-09-01 15:43:00 +0400

    feat: Refactor UserModel to extend UserEntity and streamline property initialization

--- lib/features/user/data/models/user_model.dart
+++ lib/features/user/data/models/user_model.dart
@@ -4,42 +4,28 @@ import 'package:zifromania/features/user/domain/entities/user_entity.dart';
 import 'game_stats.dart';
 import 'subscription_model.dart';
 
-class UserModel {
-  final String uid;
-  final String? displayName;
-  final String? email;
-  final String? photoURL;
-  final int coins;
-  final int level;
-  final int xp;
-  final int xpForNextLevel;
-  final bool hasActiveSubscription;
-  final List<String> achievements;
-  final List<String> completedTasks;
-  final SubscriptionModel subscription;
-  final List<String> playedDates;
-  final int currentStreak;
-  final int longestStreak;
-  final GameStats gameStats;
-
+class UserModel extends UserEntity {
   const UserModel({
-    required this.uid,
-    this.displayName,
-    this.email,
-    this.photoURL,
-    this.coins = 0,
-    this.level = 1,
-    this.xp = 0,
-    this.xpForNextLevel = 10,
-    this.hasActiveSubscription = false,
-    this.achievements = const <String>[],
-    this.completedTasks = const <String>[],
-    required this.subscription,
-    this.playedDates = const <String>[],
-    this.currentStreak = 0,
-    this.longestStreak = 0,
-    this.gameStats = const GameStats(),
+    required super.uid,
+    super.displayName,
+    super.email,
+    super.photoUrl,
+    super.coins = 0,
+    super.level = 1,
+    super.xp = 0,
+    super.xpForNextLevel = 10,
+    super.hasActiveSubscription = false,
+    super.achievements = const <String>[],
+    super.completedTasks = const <String>[],
+    required super.subscription,
+    super.playedDates = const <String>[],
+    super.currentStreak = 0,
+    super.longestStreak = 0,
+    super.gameStats = const GameStats(),
   });
+
+  @override
+  GameStats get gameStats => super.gameStats as GameStats;
 
   /// Firebase User + (isteğe bağlı) Firestore profil məlumatını birləşdirir
   factory UserModel.fromFirebase({
@@ -51,7 +37,7 @@ class UserModel {
       uid: user.uid,
       displayName: user.displayName ?? map['displayName'],
       email: user.email ?? map['email'],
-      photoURL: user.photoURL ?? map['photoURL'],
+      photoUrl: user.photoURL ?? map['photoURL'],
       coins: map['coins'] ?? 0,
       level: map['level'] ?? 1,
       xp: map['xp'] ?? 0,
@@ -72,7 +58,7 @@ class UserModel {
       uid: entity.uid,
       displayName: entity.displayName,
       email: entity.email,
-      photoURL: entity.photoUrl,
+      photoUrl: entity.photoUrl,
       coins: entity.coins,
       level: entity.level,
       xp: entity.xp,
@@ -84,30 +70,7 @@ class UserModel {
       playedDates: entity.playedDates,
       currentStreak: entity.currentStreak,
       longestStreak: entity.longestStreak,
-      gameStats: GameStats.fromEntity(
-        entity.gameStats,
-      ),
-    );
-  }
-
-  UserEntity toEntity() {
-    return UserEntity(
-      uid: uid,
-      displayName: displayName,
-      email: email,
-      photoUrl: photoURL,
-      coins: coins,
-      level: level,
-      xp: xp,
-      xpForNextLevel: xpForNextLevel,
-      hasActiveSubscription: hasActiveSubscription,
-      achievements: achievements,
-      completedTasks: completedTasks,
-      subscription: subscription.toEntity(),
-      playedDates: playedDates,
-      currentStreak: currentStreak,
-      longestStreak: longestStreak,
-      gameStats: gameStats.toEntity(),
+      gameStats: GameStats.fromEntity(entity.gameStats),
     );
   }
 
@@ -116,7 +79,7 @@ class UserModel {
       'uid': uid,
       'displayName': displayName,
       'email': email,
-      'photoURL': photoURL,
+      'photoURL': photoUrl,
       'coins': coins,
       'level': level,
       'xp': xp,
@@ -132,14 +95,12 @@ class UserModel {
     };
   }
 
-  factory UserModel.fromMap(
-    Map<String, dynamic> map,
-  ) {
+  factory UserModel.fromMap(Map<String, dynamic> map) {
     return UserModel(
       uid: map['uid'] ?? '',
       displayName: map['displayName'],
       email: map['email'],
-      photoURL: map['photoURL'],
+      photoUrl: map['photoURL'],
       coins: map['coins'] ?? 0,
       level: map['level'] ?? 1,
       xp: map['xp'] ?? 0,
@@ -147,13 +108,11 @@ class UserModel {
       hasActiveSubscription: map['hasActiveSubscription'] ?? false,
       achievements: (map['achievements'] as List<dynamic>?)?.cast<String>() ?? const [],
       completedTasks: (map['completedTasks'] as List<dynamic>?)?.cast<String>() ?? const [],
-      subscription: SubscriptionModel.fromMap(map['subscription']),
+      subscription: SubscriptionModel.fromMap(map['subscription'] ?? {}),
       playedDates: (map['playedDates'] as List<dynamic>?)?.cast<String>() ?? const [],
       currentStreak: map['currentStreak'] ?? 0,
       longestStreak: map['longestStreak'] ?? 0,
-      gameStats: GameStats.fromMap(
-        map['gameStats'],
-      ),
+      gameStats: GameStats.fromMap(map['gameStats'] ?? {}),
     );
   }
 
@@ -161,7 +120,7 @@ class UserModel {
     String? uid,
     String? displayName,
     String? email,
-    String? photoURL,
+    String? photoUrl,
     int? coins,
     int? level,
     int? xp,
@@ -179,7 +138,7 @@ class UserModel {
       uid: uid ?? this.uid,
       displayName: displayName ?? this.displayName,
       email: email ?? this.email,
-      photoURL: photoURL ?? this.photoURL,
+      photoUrl: photoUrl ?? this.photoUrl,
       coins: coins ?? this.coins,
       level: level ?? this.level,
       xp: xp ?? this.xp,
