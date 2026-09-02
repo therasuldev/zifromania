b1a4cf7c3ccb5dde8b8d549db1bae227f3de97b1
therasuldev <rasul.ramixanov@gmail.com>
2026-09-02 16:44:28 +0400

    refactor: Update import statements to use package syntax for consistency

--- lib/features/daily_reward/daily_reward_module.dart
+++ lib/features/daily_reward/daily_reward_module.dart
@@ -1,11 +1,12 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:zifromania/core/providers/shared_preferences_provider.dart';
 
-import 'data/datasources/daily_reward_local_datasource.dart';
-import 'data/repositories/daily_reward_repository_impl.dart';
-import 'domain/repositories/daily_reward_repository.dart';
-import 'domain/usecases/claim_daily_reward_usecase.dart';
-import 'domain/usecases/get_daily_reward_status_usecase.dart';
+import 'package:zifromania/features/daily_reward/data/datasources/daily_reward_local_datasource.dart';
+import 'package:zifromania/features/daily_reward/data/datasources/daily_reward_local_datasource_impl.dart';
+import 'package:zifromania/features/daily_reward/data/repositories/daily_reward_repository_impl.dart';
+import 'package:zifromania/features/daily_reward/domain/repositories/daily_reward_repository.dart';
+import 'package:zifromania/features/daily_reward/domain/usecases/claim_daily_reward_usecase.dart';
+import 'package:zifromania/features/daily_reward/domain/usecases/get_daily_reward_status_usecase.dart';
 
 final dailyRewardLocalDataSourceProvider = Provider<DailyRewardLocalDataSource>((ref) {
   return DailyRewardLocalDataSourceImpl(ref.watch(sharedPreferencesProvider));
