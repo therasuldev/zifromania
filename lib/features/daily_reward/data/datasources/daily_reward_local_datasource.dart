acf6df1f45002c18efe519e7a5c6e4fccafab880
therasuldev <rasul.ramixanov@gmail.com>
2026-09-02 16:44:41 +0400

    refactor: Remove unused implementation of DailyRewardLocalDataSource

--- lib/features/daily_reward/data/datasources/daily_reward_local_datasource.dart
+++ lib/features/daily_reward/data/datasources/daily_reward_local_datasource.dart
@@ -1,24 +1,4 @@
-import 'package:zifromania/core/services/shared_preferences_service.dart';
-
 abstract class DailyRewardLocalDataSource {
   Future<int> getLastClaimMillis();
   Future<void> saveLastClaimMillis(int millis);
-}
-
-class DailyRewardLocalDataSourceImpl implements DailyRewardLocalDataSource {
-  final PreferencesService prefs;
-
-  DailyRewardLocalDataSourceImpl(this.prefs);
-
-  static const _kLastClaimKey = 'lastClaimMillis';
-
-  @override
-  Future<int> getLastClaimMillis() async {
-    return prefs.getInt(_kLastClaimKey) ?? 0;
-  }
-
-  @override
-  Future<void> saveLastClaimMillis(int millis) async {
-    await prefs.setInt(_kLastClaimKey, millis);
-  }
 }
