a01cbdf9dc33717c14194c0f62f9edec837ef915
therasuldev <rasul.ramixanov@gmail.com>
2026-09-03 20:54:18 +0400

    fix: Update import statements to use package paths for consistency

--- lib/features/task/task_module.dart
+++ lib/features/task/task_module.dart
@@ -2,13 +2,13 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:zifromania/core/providers/firebase_provider.dart';
 import 'package:zifromania/features/user/user_module.dart';
 
-import 'data/datasource/task_datasource.dart';
-import 'data/datasource/task_datasource_impl.dart';
-import 'data/repositories/task_repository_impl.dart';
-import 'domain/repositories/task_repository.dart';
-import 'domain/usecases/complete_task_usecase.dart';
-import 'domain/usecases/get_completed_tasks_usecase.dart';
-import 'domain/usecases/get_pending_tasks_usecase.dart';
+import 'package:zifromania/features/task/data/datasource/task_datasource.dart';
+import 'package:zifromania/features/task/data/datasource/task_datasource_impl.dart';
+import 'package:zifromania/features/task/data/repositories/task_repository_impl.dart';
+import 'package:zifromania/features/task/domain/repositories/task_repository.dart';
+import 'package:zifromania/features/task/domain/usecases/complete_task_usecase.dart';
+import 'package:zifromania/features/task/domain/usecases/get_completed_tasks_usecase.dart';
+import 'package:zifromania/features/task/domain/usecases/get_pending_tasks_usecase.dart';
 
 final taskRemoteDataSourceProvider = Provider<TaskRemoteDataSource>((ref) {
   final firestore = ref.watch(firebaseFirestoreProvider);
