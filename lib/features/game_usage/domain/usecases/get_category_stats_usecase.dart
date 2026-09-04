92f67b337cd7826505d328c10ef2ecf36fc95311
therasuldev <rasul.ramixanov@gmail.com>
2026-09-04 13:51:55 +0400

    feat: Implement GetCategoryStatsUseCase for category statistics retrieval

---
+++ lib/features/game_usage/domain/usecases/get_category_stats_usecase.dart
@@ -0,0 +1,29 @@
+import 'package:zifromania/domain/entities/enums.dart';
+import 'package:zifromania/features/game_usage/domain/entities/category_stats.dart';
+import 'package:zifromania/features/game_usage/domain/repositories/game_usage_repositories.dart';
+
+final class GetCategoryStatsUseCase {
+  const GetCategoryStatsUseCase(this.repository);
+
+  final GameUsageRepository repository;
+
+  CategoryStats call(GameCategory category) {
+    final date = DateTime.now();
+    final subscriptionType = repository.getSubscriptionType();
+    final categoryLimit = repository.getCategoryLimit(subscriptionType, category);
+    final dailyRequestCount = repository.getDailyRequestCount(category, date);
+    final flexibleGamesAvailable = repository.getFlexibleGamesCount(date);
+    final remainingNormalGames = (categoryLimit - dailyRequestCount).clamp(0, categoryLimit);
+
+    return CategoryStats(
+      subscriptionType: subscriptionType,
+      category: category,
+      dailyRequestCount: dailyRequestCount,
+      categoryLimit: categoryLimit,
+      flexibleGamesAvailable: flexibleGamesAvailable,
+      remainingNormalGames: remainingNormalGames,
+      canPlayGame: dailyRequestCount < categoryLimit || flexibleGamesAvailable > 0,
+      totalAvailableGames: remainingNormalGames + flexibleGamesAvailable,
+    );
+  }
+}
