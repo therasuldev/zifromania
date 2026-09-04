0692cf649d03ee2f09f52b12a8f50e08c5531172
therasuldev <rasul.ramixanov@gmail.com>
2026-09-04 13:48:05 +0400

    feat: Add GetCategoryStatsUseCase and categoryStatsProvider for category statistics retrieval

--- lib/features/game_usage/game_usage_module.dart
+++ lib/features/game_usage/game_usage_module.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:zifromania/core/providers/shared_preferences_provider.dart';
+import 'package:zifromania/domain/entities/enums.dart';
 import 'package:zifromania/features/user/user_module.dart';
 
 import 'package:zifromania/features/game_usage/data/datasource/game_usage_local_datasource.dart';
@@ -8,9 +9,11 @@ import 'package:zifromania/features/game_usage/data/datasource/question_local_da
 import 'package:zifromania/features/game_usage/data/datasource/question_local_datasource_impl.dart';
 import 'package:zifromania/features/game_usage/data/repositories/game_usage_repositories_impl.dart';
 import 'package:zifromania/features/game_usage/data/repositories/question_repository_impl.dart';
+import 'package:zifromania/features/game_usage/domain/entities/category_stats.dart';
 import 'package:zifromania/features/game_usage/domain/repositories/question_repository.dart';
 import 'package:zifromania/features/game_usage/domain/usecases/can_play_game_usecase.dart';
 import 'package:zifromania/features/game_usage/domain/usecases/generate_question.dart';
+import 'package:zifromania/features/game_usage/domain/usecases/get_category_stats_usecase.dart';
 import 'package:zifromania/features/game_usage/domain/usecases/play_game_usecase.dart';
 
 final gameUsageLocalDataSourceProvider = Provider<GameUsageLocalDataSource>(
@@ -44,4 +47,12 @@ final generateQuestionsUseCaseProvider = Provider<GenerateQuestionsUseCase>((ref
     playGameUseCase: ref.watch(playGameUseCaseProvider),
     userRepository: ref.watch(userRepositoryProvider),
   );
+});
+
+final getCategoryStatsUseCaseProvider = Provider<GetCategoryStatsUseCase>((ref) {
+  return GetCategoryStatsUseCase(ref.watch(gameUsageRepositoryProvider));
+});
+
+final categoryStatsProvider = Provider.family<CategoryStats, GameCategory>((ref, category) {
+  return ref.watch(getCategoryStatsUseCaseProvider).call(category);
 });
