5fae08dcf4817bdcc6c0d7fd44f8875ed78f64c0
therasuldev <rasul.ramixanov@gmail.com>
2026-09-10 19:58:32 +0400

    fix: Refactor signInWithGoogle method to improve error handling and state management

--- lib/features/auth/presentation/providers/auth_provider.dart
+++ lib/features/auth/presentation/providers/auth_provider.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:zifromania/core/errors/exceptions.dart';
 import 'package:zifromania/features/auth/auth_module.dart';
 import 'package:zifromania/features/user/data/models/user_model.dart';
 
@@ -12,10 +13,18 @@ class AuthNotifier extends AsyncNotifier<UserModel?> {
   Future<void> signInWithGoogle() async {
     state = const AsyncLoading();
 
-    state = await AsyncValue.guard(() async {
+    try {
       final usecase = ref.read(signInWithGoogleUseCaseProvider);
-      return await usecase.call();
-    });
+      final user = await usecase.call();
+
+      state = AsyncData(user);
+    } on GoogleSignInCancelledException {
+      // user canceled the sign-in process, so we can just set the state to null or handle it accordingly
+      // this is not an error.
+      state = const AsyncData(null);
+    } catch (e, st) {
+      state = AsyncError(e, st);
+    }
   }
 
   Future<void> signOut() async {
