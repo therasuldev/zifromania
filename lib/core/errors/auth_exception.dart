4fd381cd11e11a9be5003ad8a2d98046b83d01c4
therasuldev <rasul.ramixanov@gmail.com>
2026-09-14 22:40:42 +0400

    refactor: Update exception classes to use localized error messages

--- lib/core/errors/auth_exception.dart
+++ lib/core/errors/auth_exception.dart
@@ -1,9 +1,18 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:zifromania/core/errors/app_exception.dart';
 
 class GoogleSignInCancelledException extends AppException {
   GoogleSignInCancelledException({
-    String message = 'Google ilə daxil olma ləğv edildi.',
-    Object? error,
-    StackTrace? stackTrace,
-  }) : super(message, error: error, stackTrace: stackTrace);
+    String? message,
+    super.error,
+    super.stackTrace,
+  }) : super(message ?? 'error.google_sign_in_cancelled'.tr());
+}
+
+class FirebaseUserNotFoundException extends AppException {
+  FirebaseUserNotFoundException({
+    String? message,
+    super.error,
+    super.stackTrace,
+  }) : super(message ?? 'error.firebase_user_not_found'.tr());
 }
