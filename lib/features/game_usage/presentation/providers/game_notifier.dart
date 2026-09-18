20b5d213a1724e973b7f1b0ae90164a8cd71aad6
therasuldev <rasul.ramixanov@gmail.com>
2026-09-18 23:00:19 +0400

    refactor: Remove unused GameConfig and GameState classes from GameNotifier

--- lib/features/game_usage/presentation/providers/game_notifier.dart
+++ lib/features/game_usage/presentation/providers/game_notifier.dart
@@ -5,97 +5,9 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:zifromania/core/errors/exceptions.dart';
 import 'package:zifromania/core/utils/cancel_token.dart';
 import 'package:zifromania/domain/entities/enums.dart';
-import 'package:zifromania/domain/entities/math_question.dart';
+import 'package:zifromania/features/game_usage/domain/entities/game_config.dart';
+import 'package:zifromania/features/game_usage/domain/entities/game_state.dart';
 import 'package:zifromania/features/game_usage/game_usage_module.dart';
-import 'package:zifromania/features/title/data/models/title_model.dart';
-
-class GameConfig {
-  const GameConfig({required this.category, required this.paidWithCoin});
-
-  final GameCategory category;
-  final bool paidWithCoin;
-
-  @override
-  bool operator ==(Object other) =>
-      other is GameConfig && other.category == category && other.paidWithCoin == paidWithCoin;
-
-  @override
-  int get hashCode => Object.hash(category, paidWithCoin);
-}
-
-class GameState {
-  const GameState({
-    this.questions = const [],
-    this.currentQuestionIndex = 0,
-    this.score = 0,
-    this.xpEarned = 0,
-    this.secondsRemaining = 0,
-    this.isLoading = false,
-    this.isGameActive = false,
-    this.lastSelectedAnswer,
-    this.lastAnsweredQuestionIndex,
-    this.isLastAnswerCorrect,
-    this.appException,
-    this.gameStartTime,
-    this.showResultDialog = false,
-    this.gameCategory,
-    this.newlyEarnedTitles = const [],
-  });
-
-  final List<MathQuestion> questions;
-  final int currentQuestionIndex;
-  final int score;
-  final int xpEarned;
-  final int secondsRemaining;
-  final bool isLoading;
-  final bool isGameActive;
-  final int? lastSelectedAnswer;
-  final int? lastAnsweredQuestionIndex;
-  final bool? isLastAnswerCorrect;
-  final AppException? appException;
-  final DateTime? gameStartTime;
-  final bool showResultDialog;
-  final GameCategory? gameCategory;
-  final List<TitleModel> newlyEarnedTitles;
-
-  MathQuestion? get currentQuestion =>
-      currentQuestionIndex < questions.length ? questions[currentQuestionIndex] : null;
-
-  GameState copyWith({
-    List<MathQuestion>? questions,
-    int? currentQuestionIndex,
-    int? score,
-    int? xpEarned,
-    int? secondsRemaining,
-    bool? isLoading,
-    bool? isGameActive,
-    int? lastSelectedAnswer,
-    int? lastAnsweredQuestionIndex,
-    bool? isLastAnswerCorrect,
-    AppException? appException,
-    DateTime? gameStartTime,
-    bool? showResultDialog,
-    GameCategory? gameCategory,
-    List<TitleModel>? newlyEarnedTitles,
-  }) =>
-      GameState(
-        questions: questions ?? this.questions,
-        currentQuestionIndex: currentQuestionIndex ?? this.currentQuestionIndex,
-        score: score ?? this.score,
-        xpEarned: xpEarned ?? this.xpEarned,
-        secondsRemaining: secondsRemaining ?? this.secondsRemaining,
-        isLoading: isLoading ?? this.isLoading,
-        isGameActive: isGameActive ?? this.isGameActive,
-        lastSelectedAnswer: lastSelectedAnswer,
-        lastAnsweredQuestionIndex: lastAnsweredQuestionIndex,
-        isLastAnswerCorrect: isLastAnswerCorrect,
-        appException: appException,
-        gameStartTime: gameStartTime ?? this.gameStartTime,
-        showResultDialog: showResultDialog ?? this.showResultDialog,
-        gameCategory: gameCategory ?? this.gameCategory,
-        newlyEarnedTitles: newlyEarnedTitles ?? this.newlyEarnedTitles,
-      );
-}
 
 class GameNotifier extends Notifier<GameState> {
   Timer? _timer;
@@ -111,9 +23,13 @@ class GameNotifier extends Notifier<GameState> {
     return const GameState();
   }
 
-  Future<void> start(String userId,
-      {required GameCategory category, required bool paidWithCoin}) async {
-    _config = GameConfig(category: category, paidWithCoin: paidWithCoin);
+  Future<void> start(
+    String userId, {
+    required GameCategory category,
+    required bool paidWithCoin,
+  }) async {
+    _config = (category: category, paidWithCoin: paidWithCoin);
+
     _timer?.cancel();
     _cancelToken?.cancel();
     final token = _cancelToken = CancelToken();
