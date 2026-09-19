28f1dc03ca89fae72aadb419a8d7ca5f4f4a65a7
therasuldev <rasul.ramixanov@gmail.com>
2026-09-19 22:46:35 +0400

    feat(game_usage): add timer indicator widget

---
+++ lib/features/game_usage/presentation/widgets/game/timer_indicator.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class TimerIndicator extends StatelessWidget {
+  final int secondsRemaining;
+
+  const TimerIndicator({super.key, required this.secondsRemaining});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Icon(Icons.timer, color: secondsRemaining < 10 ? Colors.red : Colors.green),
+        const SizedBox(width: 8),
+        AnimatedSwitcher(
+          duration: const Duration(milliseconds: 200),
+          transitionBuilder: (child, animation) {
+            // You can change this transition as desired.
+            return ScaleTransition(
+              scale: animation,
+              child: child,
+            );
+          },
+          child: Text(
+            '$secondsRemaining',
+            key: ValueKey<int>(secondsRemaining),
+            style: TextStyle(
+              fontFamily: 'Scabber',
+              fontSize: 25,
+              fontWeight: FontWeight.bold,
+              color: secondsRemaining < 10 ? Colors.red : Colors.green,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
