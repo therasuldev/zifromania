0f762d35f6b8d530ab765c9b8ffc7f22677c14dd
therasuldev <rasul.ramixanov@gmail.com>
2026-09-19 22:41:47 +0400

    refactor(game_usage): update import paths for domain entities

--- lib/features/game_usage/data/repositories/question_repository_impl.dart
+++ lib/features/game_usage/data/repositories/question_repository_impl.dart
@@ -1,5 +1,5 @@
-import 'package:zifromania/domain/entities/enums.dart';
-import 'package:zifromania/domain/entities/math_question.dart';
+import 'package:zifromania/features/game_usage/domain/entities/game_category.dart';
+import 'package:zifromania/features/game_usage/domain/entities/math_question.dart';
 import 'package:zifromania/features/game_usage/data/datasource/question_local_datasource.dart';
 import 'package:zifromania/features/game_usage/domain/repositories/question_repository.dart';
 
