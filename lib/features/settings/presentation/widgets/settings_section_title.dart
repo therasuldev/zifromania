18d6c6b12dd3d932113421b0d70faa4c3e4217f2
therasuldev <rasul.ramixanov@gmail.com>
2026-09-19 22:49:27 +0400

    refactor(settings): update constants import path

--- lib/features/settings/presentation/widgets/settings_section_title.dart
+++ lib/features/settings/presentation/widgets/settings_section_title.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:zifromania/domain/entities/constant.dart';
+import 'package:zifromania/shared/constants/app_constants.dart';
 
 class SettingsSectionTitle extends StatelessWidget {
   final String title;
